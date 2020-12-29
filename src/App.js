177367f5276a201b34b6592dde43c6d835518c72
Jennifer Lim <jenniferbachtiar@gmail.com>
2020-12-29 15:16:39 +0700

    add cities

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
-import img1 from './assets/image1.png'
+import Home from './containers/Home';
 function App() {
   return (
-    <div className="App bg-blue-200 h-screen flex items-center justify-center">
-      <header className="App-header">
-        <div>
-        <img src={img1}/>
-        </div>
-        <a> Hello my name is Jennifer</a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/"> 
+        <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
