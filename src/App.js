9573dc010024c50185baea4c8e2887e5c2737758
Jennifer Lim <jenniferbachtiar@gmail.com>
2020-12-29 14:57:21 +0700

    remove node modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import img1 from './assets/img1.jpg'
-
+import img1 from './assets/image1.png'
 function App() {
   return (
     <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
         <div>
-        <img src={image1}/>
+        <img src={img1}/>
         </div>
         <a> Hello my name is Jennifer</a>
       </header>
